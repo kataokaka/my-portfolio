31ffd0611ca6a98fbfa1e0d963bf9f73db41f9d6
kataokakka <kataokak1215@gmail.com>
2021-10-23 00:15:10 +0900

    Myportfolio

---
+++ myportfolio.js
@@ -0,0 +1,6 @@
+const titleButton = document.getElementById("titleButton");
+const message = document.getElementById("message");
+
+titleButton.onclick = function(){
+     message.textContent = " MoominValleyPark!飯能にあります！絶対がっかりさせないから是非来て!";
+}
